871d64bc133d29b2699e0fe8acd5eec150be4110
VMF-HIBIKI <148671878+VMF-HIBIKI@users.noreply.github.com>
2025-12-17 22:36:42 +0800

    “新结构体”

---
+++ Assets/Scripts/Collision/ManifoldPoint.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Mathematics;
+
+public struct ManifoldPoint
+{
+    public float3 ContactA; // A 上的点（参考面上的投影或原始点）
+    public float3 ContactB; // B 上的点（被裁剪后的点）
+    public float Depth;     // 穿透深度
+    public float3 Normal;   // 法线 (A -> B)
+}
